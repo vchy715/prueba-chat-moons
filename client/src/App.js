4c3d22f0d4e942d185b9c68a869dc9c1bfcf31fd
Victoria <vickychang.3843@gmail.com>
2021-06-26 11:48:46 -0500

    feat: Initialize client

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import io from 'socket.io-client';
+const socket = io('http://localhost:4000');
+
+
+function App() {
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default App;
