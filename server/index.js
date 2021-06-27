d88cb528f4bb5cda00e4a3c5c14724ca6ea58d07
Victoria <vickychang.3843@gmail.com>
2021-06-26 22:59:00 -0500

    feat: implement socket in server

--- server/index.js
+++ server/index.js
@@ -1,23 +1,36 @@
-const express = require('express');
-
-const app = express();
-const server = require('http').Server(app);
+const server = require('http').createServer();
 const io = require('socket.io')(server, {
   cors: {
     origin: '*',
   },
 });
+const uuid = require('uuid');
 
 const { clog } = require('./helpers');
 
 const PORT = process.env.PORT || 4000;
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+const USER_JOIN_EVENT = 'user_join';
+const USER_LEAVE_EVENT = 'user_leave';
+const NEW_MESSAGE_EVENT = 'message_received';
 
 io.on('connection', (socket) => {
   clog(`${socket.id} connected`);
+  const { room, userName } = socket.handshake.query;
+  socket.join(room);
+
+  const user = { name: userName, id: socket.id };
+  io.to(room).emit(USER_JOIN_EVENT, user);
+
+  socket.on(NEW_MESSAGE_EVENT, (msg) => {
+    io.to(room).emit(NEW_MESSAGE_EVENT, {
+      ...msg,
+      id: uuid.v4(),
+    });
+  });
+
   socket.on('disconnect', () => {
+    io.to(room).emit(USER_LEAVE_EVENT);
+    socket.leave(room);
     clog(`${socket.id} disconnected`);
   });
 });
